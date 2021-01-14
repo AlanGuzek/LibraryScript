540c56fb78a1643fd0998801d9af3e70f879253f
AlanGuzek <alanguzek@student.agh.edu.pl>
2021-01-14 21:17:33 +0100

    First updates and making the ground for future code

--- library.py
+++ library.py
@@ -1,17 +1,42 @@
 import requests
+import pprint
+import pandas as pd
+import csv
 
 
-def get_books(url):
-    r = requests.get(url)
-    return r.json()
+def get_books(author="Andrzej Sapkowski", kind="książka", title="Pani Jeziora", language="polski"):
+    url = "http://data.bn.org.pl/api/bibs.json"
+    querystring = {
+        "kind": kind,
+        "author": author,
+        "title": title,
+        "language": language
+    }
+    r = requests.request("GET", url, params=querystring)
+    books = r.json()
+    books = pd.DataFrame(books['bibs'])
+    books = books[['id', 'author', 'title', 'language']]
+    return books
 
 
-def return_book(book="Wiedźmin"):
+def return_book(user_id=0, book_id=0):
     pass
 
 
-def borrow_book(book="Wiedźmin"):
-    pass
+def borrow_book(user_id=0):
+    list_of_books = get_books()
+    rb = pd.read_csv("RentedBooks.csv")
+    print(list_of_books.to_string())
+    pprint.pprint(rb)
+    i = int(input("Select your book: "))
+    append_book("RentedBooks.csv", (user_id, list_of_books[i]['id']))
+
+
+def append_book(filename, appending):
+    with open(filename, 'a+', newline='') as write_obj:
+        csv_writer = csv.writer(write_obj)
+        csv_writer.writerow(appending)
+        write_obj.close()
 
 
 def menu():
