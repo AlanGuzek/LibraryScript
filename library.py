da90aaa09d49ef46267f129c00a1fbc7af8802a0
AlanGuzek <alanguzek@student.agh.edu.pl>
2021-01-15 08:16:47 +0100

    Version 1.0 -utf-8 encoding -match author or title or both -shouldn't show any errors

--- library.py
+++ library.py
@@ -2,10 +2,11 @@ import requests
 import pprint
 import pandas as pd
 import csv
+import codecs
 import numpy as np
 
 
-def get_books(author=None, kind="książka", title=None, language="polski"):
+def get_books(author="", kind="książka", title="", language="polski"):
     url = "http://data.bn.org.pl/api/bibs.json"
     querystring = {
         "kind": kind,
@@ -13,6 +14,10 @@ def get_books(author=None, kind="książka", title=None, language="polski"):
         "title": title,
         "language": language
     }
+    if author == "":
+        querystring.pop("author")
+    if title == "":
+        querystring.pop("title")
     r = requests.request("GET", url, params=querystring)
     books = r.json()
     books = pd.DataFrame(books['bibs'])
@@ -21,24 +26,31 @@ def get_books(author=None, kind="książka", title=None, language="polski"):
 
 
 def return_book():
-    print("Which one you want to return?")
-    rb = pd.read_csv("RentedBooks.csv", dtype={'Book_ID': 'Int64', 'User_ID': 'Int64'})
+    rb = pd.read_csv("RentedBooks.csv", dtype={'Book_ID': 'Int64', 'User_ID': 'Int64'}, encoding='utf-8')
+    empty = True
     for i in range(len(rb['Book_ID'])):
         if rb['User_ID'][i] == user_id:
             print(f'[{i}] {rb["Title"][i]}\n')
-    i = int(input())
-    book_id = rb['Book_ID'][i]
-    pass
-    delete_book("RentedBooks.csv", book_id)
+            empty = False
+    if not empty:
+        try:
+            i = int(input("Which one you want to return: "))
+            book_id = rb['Book_ID'][i]
+            delete_book("RentedBooks.csv", book_id)
+        except:
+            pass
+    else:
+        print("You have nothing to return")
 
 
 def borrow_book():
     author = input("Type author: ")
     title = input("Type title: ")
     list_of_books = get_books(author=author, title=title)
-    rb = pd.read_csv("RentedBooks.csv", dtype={'Book_ID': 'Int64'})
+    rb = pd.read_csv("RentedBooks.csv", dtype={'Book_ID': 'Int64'}, encoding='utf-8')
     for i in range(len(list_of_books['id'])):
-        for na_id in rb['Book_ID']:
+        for na_id in rb['Book_ID'][1:]:
+            na_id = int(na_id)
             if list_of_books['id'][i] == na_id:
                 list_of_books = list_of_books.drop(index=i)
                 break
@@ -46,10 +58,13 @@ def borrow_book():
         print("There are no matching books!")
     else:
         print(list_of_books.to_string())
-        i = int(input("Select your book: "))
-        book_id = int(list_of_books['id'][i])
-        book_title = list_of_books['title'][i]
-        append_book("RentedBooks.csv", (user_id, book_id, book_title))
+        try:
+            i = int(input("Select your book: "))
+            book_id = int(list_of_books['id'][i])
+            book_title = list_of_books['title'][i]
+            append_book("RentedBooks.csv", (user_id, book_id, book_title))
+        except:
+            borrow_book()
 
 
 def delete_book(filename, book_id):
@@ -70,7 +85,7 @@ def delete_book(filename, book_id):
 
 # code from internet, not my own
 def append_book(filename, appending):
-    with open(filename, 'a+', newline='\n') as write_obj:
+    with codecs.open(filename, 'a+', encoding='utf-8') as write_obj:
         csv_writer = csv.writer(write_obj)
         csv_writer.writerow(appending)
         write_obj.close()
